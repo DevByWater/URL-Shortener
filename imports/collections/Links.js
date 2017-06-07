3edf15e867a0ba637e543280f67551c2fbfd7d8c
DevByWater <egaraudy@gmail.com>
2017-06-07 15:03:18 -0600

    create url validation with meteor check helper

--- imports/collections/Links.js
+++ imports/collections/Links.js
@@ -1,8 +1,13 @@
 import { Mongo } from 'meteor/mongo'
+import { check, Match } from 'meteor/check'
+import validUrl from 'valid-url'
+
 
 Meteor.methods({
     'links.insert': function(url) {
         console.log('links insert: ', url )
+        validUrl.isUri(url)
+        check(url, Match.Where(url => validUrl.isUri(url)))
     }
 })
 
