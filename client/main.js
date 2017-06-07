ee45e5a5dab0f7f7785dd6007d9275372326a1d4
DevByWater <egaraudy@gmail.com>
2017-06-07 11:51:55 -0600

    create form component

--- client/main.js
+++ client/main.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom'
 import { Meteor } from 'meteor/meteor';
 
 import Header from './components/header'
+import LinkCreate from './components/link_create'
 
 
 const App = () => {
   return (
     <div>
         <Header />
+        <LinkCreate />
     </div>
   )
 }
